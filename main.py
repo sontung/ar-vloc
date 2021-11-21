13e7b61c8b585bbaa0a72f0a95c1353e27c9123e
sontung <tung.nguyenson1404@gmail.com>
2021-11-21 09:22:46 +0700

    2d-3d matching4

--- main.py
+++ main.py
@@ -6,36 +6,64 @@ import cv2
 import kornia
 from colmap_io import build_descriptors
 from pathlib import Path
-# from pykdtree.kdtree import KDTree
 from scipy.spatial import KDTree
+from matplotlib import pyplot as plt
 
 
 def load_2d_queries(folder="test_images"):
-    im_names = os.listdir(folder)
-    sift_model = kornia.feature.SIFTFeature(num_features=1000)
-    im_list = []
-    for name in im_names:
-        im_name = os.path.join(folder, name)
-        im = cv2.imread(im_name, cv2.IMREAD_GRAYSCALE)
-        im = cv2.resize(im, (1536//2, 2048//2))
-        im_list.append(np.expand_dims(im, -1).astype(float))
-        # cv2.imshow("t", im)
-        # cv2.waitKey()
-        # cv2.destroyAllWindows()
-    im_list = kornia.utils.image_list_to_tensor(im_list)
-    for i in range(im_list.size(0)):
-        an_im = torch.unsqueeze(im_list[i], 0)
+    im_names_dir = "test_images/im_names.pkl"
+    desc_dir = 'test_images/keypoint_descriptors.pt'
 
-        with torch.no_grad():
-            _, _, descs = sift_model.forward(an_im)
+    my_file = Path(im_names_dir)
+    if my_file.is_file():
+        print("Loading 2D descriptors for test images at test_images/")
+        descs = torch.load(desc_dir)
+        with open("test_images/im_names.pkl", "rb") as fp:
+            im_names = pickle.load(fp)
+    else:
+        im_names = os.listdir(folder)
+        sift_model = kornia.feature.SIFTFeature(num_features=1000, device=torch.device("cpu"))
+        descs = []
+        for name in im_names:
+            im_list = []
+            im_name = os.path.join(folder, name)
+            im = cv2.imread(im_name, cv2.IMREAD_GRAYSCALE)
+            im = cv2.resize(im, (im.shape[1]//2, im.shape[0]//2))
+            im_list.append(np.expand_dims(im, -1).astype(float))
+            # cv2.imshow("t", im)
+            # cv2.waitKey()
+            # cv2.destroyAllWindows()
+            an_im = kornia.utils.image_list_to_tensor(im_list)
+            with torch.no_grad():
+                laf, response, desc = sift_model.forward(an_im)
+                descs.append(desc.cpu())
+                points = kornia.feature.laf.get_laf_center(laf)
+                px, py = kornia.feature.laf.get_laf_pts_to_draw(laf)
+                plt.figure()
+                plt.imshow(kornia.utils.tensor_to_image(an_im))
+                plt.plot(px, py, 'r')
+                plt.plot(points[0, :, 0], points[0, :, 1], 'bo')
+                plt.show()
+            break
+        descs = torch.stack(descs)
+
+        # torch.save(descs, desc_dir)
+        # with open(im_names_dir, "wb") as fp:
+        #     pickle.dump(im_names, fp)
+        # print("Saved 2D descriptors at test_images/")
+
+    return descs, im_names
 
 
 def load_3d_database():
-    my_file = Path("data/point3d_ids.pkl")
+    point3d_ids_dir = "data/point3d_ids.pkl"
+    point3d_descs_dir = "data/point3d_descs"
+
+    my_file = Path(point3d_ids_dir)
     if my_file.is_file():
         print("Loading 3D descriptors at data/")
-        point3d_desc_list = np.load("data/point3d_descs.npy")
-        with open("data/point3d_ids.pkl", "rb") as fp:
+        point3d_desc_list = np.load(f"{point3d_descs_dir}.npy")
+        with open(point3d_ids_dir, "rb") as fp:
             point3d_id_list = pickle.load(fp)
         print(f"\t{len(point3d_id_list)} 3D points with desc mat {point3d_desc_list.shape}")
     else:
@@ -50,8 +78,8 @@ def load_3d_database():
             point3d_desc_list.append(mean_desc.numpy())
         point3d_desc_list = np.vstack(point3d_desc_list)
         print("Saved 3D descriptors at data/")
-        np.save("data/point3d_descs", point3d_desc_list)
-        with open("data/point3d_ids.pkl", "wb") as fp:
+        np.save(point3d_descs_dir, point3d_desc_list)
+        with open(point3d_ids_dir, "wb") as fp:
             pickle.dump(point3d_id_list, fp)
     return point3d_id_list, point3d_desc_list
 
@@ -65,7 +93,11 @@ def matching_2d_to_3d(point3d_id_list, point3d_desc_list):
         # break
 
 
+def main():
+    desc_list, im_name_list = load_2d_queries()
+    point3d_id_list, point3d_desc_list = load_3d_database()
+    matching_2d_to_3d(point3d_id_list, point3d_desc_list)
+
+
 if __name__ == '__main__':
-    load_2d_queries()
-    p1, p2 = load_3d_database()
-    matching_2d_to_3d(p1, p2)
+    main()
