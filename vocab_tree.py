75c63f551e54cba7606e20c55ceb6acedb3bcbc6
sontung <tung.nguyenson1404@gmail.com>
2021-12-04 17:05:49 +0700

    active search2

--- vocab_tree.py
+++ vocab_tree.py
@@ -18,30 +18,60 @@ class VocabNode:
         labels = self.cluster_model.fit_predict(p3d_desc_list)
         for i in range(len(p3d_id_list)):
             self.vocab[labels[i]].append((p3d_id_list[i], p3d_desc_list[i], i))
-        return
+        cluster_centers = self.cluster_model.cluster_centers_
+        words = [Word(du, cluster_centers[du]) for du in range(cluster_centers.shape[0])]
+        return words
+
+
+class Word:
+    def __init__(self, index, descriptor):
+        self.index = index
+        self.descriptor = descriptor
 
 
 class VocabNodeForWords:
-    def __init__(self, nb_clusters):
-        self.nb_clusters = nb_clusters
+    def __init__(self, level, branching_factor):
         self.vocab = None
         self.cluster_model = None
+        self.branching_factor = branching_factor
+        self.nb_clusters = branching_factor
+        self.children = []
+        self.level = level
         return
 
-    def build(self, words):
+    def build(self, word_indices, word_descriptors):
+        next_level = self.level + 1
+        if self.level > 3:
+            return []
         self.vocab = {u: [] for u in range(self.nb_clusters)}
         self.cluster_model = MiniBatchKMeans(self.nb_clusters)
-        labels = self.cluster_model.fit_predict(words)
-        for i in range(len(words)):
-            self.vocab[labels[i]].append((words[i], i))
-        return
+        word_descriptors = np.vstack(word_descriptors)
+        labels = self.cluster_model.fit_predict(word_descriptors)
+        for i in range(len(word_indices)):
+            self.vocab[labels[i]].append((word_indices[i], word_descriptors[i]))
+
+        data = []
+        for child_id in range(self.nb_clusters):
+            child = VocabNodeForWords(next_level, self.branching_factor)
+            sub_word_indices = [word[0] for word in self.vocab[child_id]]
+            sub_word_descriptors = [word[1] for word in self.vocab[child_id]]
+            data.extend([(word_ind, self.level, child_id) for word_ind in sub_word_indices])
+            sub_data = child.build(sub_word_indices, sub_word_descriptors)
+            data.extend(sub_data)
+            self.children.append(child)
+        return data
 
 
 class VocabTree:
-    def __init__(self, p3d_id_list, p3d_desc_list, branching_factor=5):
+    def __init__(self, p3d_id_list, p3d_desc_list, branching_factor=2):
         self.v1 = VocabNode(nb_clusters=500)
-        self.v1.build(p3d_id_list, p3d_desc_list)
-        self.v2 = VocabNode(nb_clusters=branching_factor)
+        words = self.v1.build(p3d_id_list, p3d_desc_list)
+        word_indices = [word.index for word in words]
+        word_descriptors = [word.descriptor for word in words]
+
+        self.v2 = VocabNodeForWords(1, branching_factor)
+        data = self.v2.build(word_indices, word_descriptors)
+        print(len(data), len(words))
         return
 
 
@@ -60,6 +90,4 @@ if __name__ == '__main__':
     points_3d_list = []
     point3d_id_list, point3d_desc_list = build_descriptors_2d(image2pose, sfm_images_folder)
     desc_list, coord_list, im_name_list = load_2d_queries_opencv(query_images_folder)
-    print(desc_list[0].shape)
-    print(len(point3d_desc_list))
     vocab_tree = VocabTree(point3d_id_list, point3d_desc_list)
