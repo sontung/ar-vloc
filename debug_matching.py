5ad6ed78f373dd9c74ee7ec0d29ecfa2920edebf
sontung <tungns@hblab.vn>
2021-12-22 15:49:42 +0700

    better debug tool2

--- debug_matching.py
+++ debug_matching.py
@@ -7,6 +7,8 @@ from colmap_io import read_points3D_coordinates, read_images
 from feature_matching import build_descriptors_2d
 from vocab_tree import VocabTree
 from vis_utils import visualize_matching, visualize_matching_helper
+from hardnet import build_descriptors_2d as build_descriptors_2d_hardnet
+from hardnet import load_2d_queries_generic as load_2d_queries_generic_hardnet
 
 
 def draw_circle(event, x, y, flags, param):
@@ -16,13 +18,22 @@ def draw_circle(event, x, y, flags, param):
         mouseX, mouseY = x*down_scale, y*down_scale
 
 
+USING_HARDNET = False
 query_images_folder = "Test line small"
 sfm_images_dir = "sfm_ws_hblab/images.txt"
 sfm_point_cloud_dir = "sfm_ws_hblab/points3D.txt"
 sfm_images_folder = "sfm_ws_hblab/images"
 image2pose = read_images(sfm_images_dir)
 point3did2xyzrgb = read_points3D_coordinates(sfm_point_cloud_dir)
-point3d_id_list, point3d_desc_list, p3d_desc_list_multiple, point3did2descs = build_descriptors_2d(image2pose, sfm_images_folder)
+
+if USING_HARDNET:
+    using_ps = True
+    read_data = build_descriptors_2d_hardnet(image2pose,
+                                             sfm_images_folder,
+                                             using_ps=using_ps)
+    point3d_id_list, point3d_desc_list, p3d_desc_list_multiple, point3did2descs = read_data
+else:
+    point3d_id_list, point3d_desc_list, p3d_desc_list_multiple, point3did2descs = build_descriptors_2d(image2pose, sfm_images_folder)
 
 point3d_cloud = PointCloud(point3did2descs, debug=True)
 for i in range(len(point3d_id_list)):
@@ -33,14 +44,18 @@ for i in range(len(point3d_id_list)):
 
 point3d_cloud.commit(image2pose)
 vocab_tree = VocabTree(point3d_cloud)
-descriptors, coordinates, im_names, md_list, im_list, response_list = load_2d_queries_generic(query_images_folder)
-point2d_cloud = FeatureCloud()
+if USING_HARDNET:
+    read_queries = load_2d_queries_generic_hardnet(query_images_folder, using_ps=using_ps)
+    descriptors, coordinates, im_names, md_list, im_list, response_list = read_queries
+else:
+    descriptors, coordinates, im_names, md_list, im_list, response_list = load_2d_queries_generic(query_images_folder)
+point2d_cloud = FeatureCloud(im_list[0])
 
 for j in range(coordinates[0].shape[0]):
     point2d_cloud.add_point(j, descriptors[0][j], coordinates[0][j], response_list[0][j])
 point2d_cloud.nearby_feature(0, 2)
-
-image_ori = im_list[0]
+point2d_cloud.rank_feature_strengths()
+image_ori = point2d_cloud.cluster(debug=True)
 down_scale = 5
 image_ori = cv2.resize(image_ori, (image_ori.shape[1]//down_scale, image_ori.shape[0]//down_scale))
 
@@ -86,7 +101,8 @@ if INTERACTIVE:
                 data1 = point3d_cloud.matching_2d_to_3d_brute_force_no_ratio_test(point2d_cloud[ind].desc)
                 point_ind, dist, _, ratio = data1
                 RESULT_NO_RATIO[(mouseX, mouseY)] = (ind, point_ind, dist)
-                print(f"ratio test: {mouseX, mouseY} => {ind, point_ind, dist, ratio}")
+                print(f"ratio test: {mouseX, mouseY} => {ind, point_ind, dist, ratio, point2d_cloud[ind].strength_rank}"
+                      f"")
             else:
                 ind, point_ind, dist = RESULT_NO_RATIO[(mouseX, mouseY)]
             if point_ind is not None:
